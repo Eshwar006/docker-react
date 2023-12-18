b3dfb025134883fa4cdfed02f97036bfac41fcbe
Eshwar006 <em10@iitbbs.ac.in>
2023-12-18 21:56:38 +0530

    updated app name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          This is Feature branch
+          Learn React
         </a>
       </header>
     </div>
